f9171f7f9635623fed18009d4bfe9fe7b6d33763
Jsheen98 <jsheendev@gmail.com>
2023-04-14 15:40:19 -0600

    Pushing to receive help with an issue

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,26 +1,75 @@
 const router = require('express').Router()
 const { ObjectId } = require('mongoose').Types
-// const { User, Thought } = require('../../models')
+const Thought  = require('../../models/Thought')
 
 // /api/thoughts
-
 // GET: get all thoughts
+router.get('/', async (req, res) => {
+    try {
+        const thoughts = await Thought.find()
+
+        res.status(200).json(thoughts)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // GET: get one thought (by _id)
+router.get('/:thoughtId', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // POST: create a new thought (make sure to push the created thought _id to associated user's thoughts array field)
+router.post('/', async (req, res) => {
+    try {
+        const thought = await Thought.create(req.body)
+
+        res.status(200).json(thought)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // PUT: update a thought (by _id)
+router.post('/:thoughtId', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // DELETE: delete a thought (by _id)
+router.post('/:thoughtId', async (req, res) => {
+    try {
 
-
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 
 // /api/thoughts/:thoughtId/reactions
-
 // POST: create a reaction (stored in thought's reaction array)
+router.post('/:thoughtId/reactions', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 // DELETE: remove a reaction (by the reactionId)
+router.delete('/:thoughtId/reactions', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
